1db66b47fd18d3ad5b480c19ea803b25306a8d12
Victor Anderssén <victor.anderssen@gmail.com>
2022-11-12 17:16:51 +0200

    Add project files.

---
+++ cpp_playground.cpp
@@ -0,0 +1,58 @@
+﻿#include "cpp_playground.h"
+
+global_variable int Running = 1;
+global_variable HINSTANCE hinst;
+global_variable HWND hwndMain;
+
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int APIENTRY WinMain(
+    HINSTANCE hInstance, 
+    HINSTANCE hInstancePrevious, 
+    PSTR cmdline, 
+    int cmdshow
+)
+{
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WinProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = TEXT("TESTING");
+
+    RegisterClass(&wc);
+
+    // Create the window.
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,                              // Optional window styles.
+        TEXT("TESTING"),                // Window class
+        TEXT("TESTING"),                // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,// Size and position
+        NULL,                           // Parent window    
+        NULL,                           // Menu
+        hInstance,                      // Instance handle
+        NULL                            // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return (0);
+    }
+
+    ShowWindow(hwnd, cmdshow);
+
+	// Main loop
+	while (Running)
+	{
+		MSG msg = { };
+		while (GetMessage(&msg, NULL, 0, 0) > 0)
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (0);
+}
---
+++ cpp_playground.h
@@ -0,0 +1,14 @@
+﻿// cpp_playground.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+#define local_persist static 
+#define internal static 
+#define global_variable static 
+
+// TODO: Reference additional headers your program requires here.
